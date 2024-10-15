cc9557a4ddfbb3dcb18e8d7ab19ead8135f70330
dayfx <if22b206@technikum-wien.at>
2024-10-16 00:58:53 +0200

    User creation through curl script request implemented

--- src/main/java/application/User.java
+++ src/main/java/application/User.java
@@ -1,13 +1,38 @@
 package application;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class User {
 
+    @JsonProperty("Username")
     private String username;
+
+    @JsonProperty("Password")
     private String password;
+
+    public User(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+       this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public User(String username, String password) {
     this.username = username;
--- src/main/java/server/server/UserService.java
+++ src/main/java/server/server/UserService.java
@@ -1,17 +1,41 @@
 package server.server;
 
+import application.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import server.http.ContentType;
 import server.http.HttpStatus;
 
 public class UserService implements Service{
     @Override
     public Response handleRequest(Request request) {
-        if(request.getMethod().equals("POST")) {
-            System.out.println(request.getBody());
+        String method = String.valueOf(request.getMethod());
+        if(method.equals("POST")) {
+            try {
+                System.out.println(request.getBody());
 
+                ObjectMapper objectMapper = new ObjectMapper();
 
+                User user = objectMapper.readValue(request.getBody(), User.class);
+
+                String username = user.getUsername();
+                String password = user.getPassword();
+
+                System.out.println("into post it goes");
+                System.out.println("This Username: " + username);
+                System.out.println("This Password: " + password);
+
+                Response response = new Response(HttpStatus.CREATED, ContentType.JSON, request.getBody());
+                return response;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Response response = new Response(HttpStatus.BAD_REQUEST, ContentType.JSON, "Invalid JSON format");
+                return response;
+            }
+
+        } else {
+            Response response = new Response(HttpStatus.BAD_REQUEST, ContentType.JSON, request.getBody());
+            return response;
         }
-        Response response = new Response(HttpStatus.BAD_REQUEST, ContentType.JSON, request.getBody());
-        return response;
     }
 }
